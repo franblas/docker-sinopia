776024c322d0796c31405a73fe860b47c626d19a
franblas <francois_blas@hotmail.com>
2015-09-30 20:26:42 +0200

    new fix

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [ ! -f config.yaml ]; then
-  node /opt/docker-sinopia/config_gen.js
-  sed -e 's/\#listen\: localhost/listen\: 0.0.0.0/' /tmp/config.yaml > /opt/docker-sinopia/config.yaml
-fi
-cat /opt/docker-sinopia/config.yaml
 node /opt/docker-sinopia/node_modules/sinopia/bin/sinopia --config /opt/docker-sinopia/config.yaml
